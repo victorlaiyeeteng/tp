947ed318b9c4d1a394fbebe94ebc4e06b273c6f0
Elijah5399 <chia.elijah@gmail.com>
2023-10-20 02:59:41 +0800

    Fix checkstyle violations and add plans header

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -8,6 +8,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import seedu.address.commons.core.GuiSettings;
@@ -36,6 +37,8 @@ public class MainWindow extends UiPart<Stage> {
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
 
+    @FXML
+    private AnchorPane plansHeaderPlaceholder;
     @FXML
     private StackPane commandBoxPlaceholder;
 
