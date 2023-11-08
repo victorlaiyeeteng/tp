f72b3826bdbc7b57897c5087868e44008ec8e2b2
Jannice <>
2023-11-08 16:14:32 +0800

    removed unused argument

--- src/main/java/seedu/address/logic/commands/ListPlanCommand.java
+++ src/main/java/seedu/address/logic/commands/ListPlanCommand.java
@@ -3,10 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PLANS;
 
-import java.util.logging.Logger;
-
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.LogicManager;
 import seedu.address.model.Model;
 
 
@@ -17,8 +13,6 @@ public class ListPlanCommand extends Command {
 
     public static final String COMMAND_WORD = "list-plan";
     public static final String MESSAGE_SUCCESS = "Listed all plans";
-    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
-
 
     @Override
     public CommandResult execute(Model model) {
